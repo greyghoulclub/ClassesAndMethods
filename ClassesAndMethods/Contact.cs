23c846e7f5d5718fbc238dc80814c198c86947ba
15029737 <15029737@WH03390>
2023-02-28 15:30:16 +0000

    Add project files.

---
+++ ClassesAndMethods/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods
+{
+    public class Contact
+    {
+        
+        public string Name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        public string EmailAddress
+        {
+            get { return EmailAddress; }
+            set { EmailAddress = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return PhoneNumber;}
+            set { PhoneNumber = value; }
+        }
+    }
+
+    
+
+
+}
---
+++ ClassesAndMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes_and_methods
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            Contact myObj = new Contact();
+            myObj.Name = "Hope";
+            Console.WriteLine(myObj.Name);
+
+            Contact myObj = new Contact();
+            myObj.EmailAddress = "Hope@blahblah.com";
+            Console.WriteLine(myObj.EmailAddress);
+
+            Contact myObj = new Contact();
+            myObj.PhoneNumber = "6789045";
+            Console.WriteLine(myObj.PhoneNumber);
+        }
+    }
+}
